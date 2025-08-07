fda1fa9dd37be90aee6dbd7b4c3bf0dc02d7b198
Qixian Wang <143909909+Qixian-Wang@users.noreply.github.com>
2025-08-07 15:42:17 -0500

    Update yolo_training.py

--- Yolo_code/yolo_training.py
+++ Yolo_code/yolo_training.py
@@ -1,33 +1,30 @@
 from ultralytics import YOLO
 
-path = 'D:/Python/Projects/tool_labeling'
-best_weights = 'D:/Python/Projects/tool_labeling/runs/detect/yolo11/weights/best.pt'
-
-train = 'images/images_train'
-val =   'images/images_val'
+best_weights = 'D:/Python/Projects/tool_labeling/runs/detect/yolo1117/weights/last.pt'
 
 if __name__ == '__main__':
 
-    mode = "training"
-    # mode = "testing"
+    # mode = "training"
+    mode = "testing"
 
     if mode == "training":
         # training
         model = YOLO('yolo11.yaml')
         results = model.train(
             data='bio_tool.yaml',         # data yaml file
-            epochs=80,                   # training epochs
+            epochs=3,                   # training epochs
             imgsz=960,                    # image size
-            batch=8,                     # batch size
+            batch=6,                     # batch size
             device=[0],                   # GPU number
             name='yolo11',                # experiment name
             )
+            
     elif mode == "testing":
         # testing
         model = YOLO(best_weights)
         results = model.predict(
-            source=val,  # validation image folder
-            imgsz=1600,
+            source='C:/Users/Qixian/Desktop/lab_tool_data/All_figures/images/val',  # validation image folder
+            imgsz=960,
             conf=0.5,
             save=True,
             project='runs/val_preds',
